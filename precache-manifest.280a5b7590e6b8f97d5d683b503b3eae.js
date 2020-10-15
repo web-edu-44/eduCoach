8cf86f32878c2e2fa1c97657ae0462d158054767
Mohammed Seidu <seyid24itc@gmail.com>
2020-10-15 15:26:52 +0000

    Updates

---
+++ precache-manifest.280a5b7590e6b8f97d5d683b503b3eae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e297b771f93dbf54b8169798ba4869",
+    "url": "/eduCoach/index.html"
+  },
+  {
+    "revision": "c95ad0e79d11b0756281",
+    "url": "/eduCoach/static/css/main.fab1086c.chunk.css"
+  },
+  {
+    "revision": "b92e0ab2effa31e847d7",
+    "url": "/eduCoach/static/js/2.585dad1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eduCoach/static/js/2.585dad1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95ad0e79d11b0756281",
+    "url": "/eduCoach/static/js/main.b0c8137a.chunk.js"
+  },
+  {
+    "revision": "bbf3da3e0398b4f14ff7",
+    "url": "/eduCoach/static/js/runtime-main.7b25f8cf.js"
+  },
+  {
+    "revision": "e9155a6435d0c28a492a456192dfe353",
+    "url": "/eduCoach/static/media/block-chat.e9155a64.png"
+  },
+  {
+    "revision": "c619f9a8e3f9a3c6e33655a4e60b1012",
+    "url": "/eduCoach/static/media/booking.c619f9a8.png"
+  },
+  {
+    "revision": "efe1f7ecdcda36c9ffacae4512dd0f5a",
+    "url": "/eduCoach/static/media/pexels-andrea-piacquadio-3932232.efe1f7ec.jpg"
+  }
+]);
