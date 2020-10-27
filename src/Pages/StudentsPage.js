de6fb45e005289dec7ba57ebf9e989da1a8cd982
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2020-10-27 21:30:54 +0200

    student page

--- src/Pages/StudentsPage.js
+++ src/Pages/StudentsPage.js
@@ -4,10 +4,21 @@ import './Pages.css'
 export const Students = ()=> {
     return (
         <div className='pageContainer'>
-            <h1>Students Page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, </p>
-            <p>  adipisicing elit. Minima eum dicta iusto necessitatibus </p>
-            <p>  a dolores, deserunt nemo maxime expedita quas.</p>
+            <header className='studentHeader'>
+                <div>
+                    <h1>Career Services and Alumni Advice</h1>
+                    <ul>
+                        <li>Assess your values, interests, skills and strengths</li>
+                        <li>Discover career options both inside and outside academia</li>
+                        <li>Build a professional network</li>
+                        <li>Create/revise CVs, resumes and cover letters</li>
+                        <li>Prepare for upcoming interviews</li>
+                        <li>Refine communications and networking skills</li>
+                    </ul>
+                </div>
+                <div className='studentImg'><div className='cover'></div></div>
+            </header>
+            
         </div>
 
     )
